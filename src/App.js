7a41dc74608ca284c2c416d2ee27fc1f9cc20117
amandena <manda.panda91@yahoo.com>
2020-06-22 23:45:23 -0400

    imported new components into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
+import Form from './components/Form'
+import Weather from './components/Weather'
 
 function App() {
   return (
     <div className="App">
       <h3>WEATHER APP</h3>
+      <Form/>
+      <Weather/>
     </div>
   )
 }
