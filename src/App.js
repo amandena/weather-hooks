23038021ed6bec633331a526905e777d041acf48
amandena <manda.panda91@yahoo.com>
2020-06-24 23:43:59 -0400

    added api key, created fetchData function and added prop to form component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Form from './components/Form'
 import Weather from './components/Weather'
 
 function App() {
+  const [weather, setWeather] = useState([])
+  const APIKEY = '24ffdfdd664d7e8bbc57efbbac334b74'
+
+  async function fetchData(e) {
+    e.preventDefault()
+    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${APIKEY}`)
+      .then( res => res.json())
+      .then(data => data)
+      setWeather({
+        data: apiData
+      })
+  }
+
   return (
     <div className="App">
       <h3>WEATHER APP</h3>
-      <Form/>
+      <Form getWeather={fetchData}/>
+      {console.log(weather.data)}
       <Weather/>
     </div>
   )
