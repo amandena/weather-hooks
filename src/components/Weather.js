7166d6c964cbeba3c493885ff95ba95c0612ac4b
amandena <manda.panda91@yahoo.com>
2020-06-25 23:59:48 -0400

    added more to p tags

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,8 +4,8 @@ const Weather = ({description, city, country, error, temperature}) => {
   return(
     <div>
       {city && country && <p>{city}, {country}</p>}
-      {temperature && <p>{temperature}</p>}
-      {description && <p> {description}</p>}
+      {temperature && <p>{temperature}  °F</p>}
+      {description && <p> Conditions: {description}</p>}
       {error && <p>{error}</p>}
     </div>
   )
